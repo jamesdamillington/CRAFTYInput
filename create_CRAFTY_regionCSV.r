8f625b657981d51aaaf0f6794e68917e76c06133
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-07-07 12:41:59 +0100

    added code for double cropping

--- create_CRAFTY_regionCSV.r
+++ create_CRAFTY_regionCSV.r
@@ -81,11 +81,6 @@ landCover.r <-flip(landCover.r, 'y')
 Lpr.r<-flip(Lpr.r, 'y')
 
 
-#create reclassify matrix: muni ids and a random value (uniform for entire muni)
-set.seed(1)
-
-statecsv<-read.csv("Municipality_area_and_IBGE_code_number.csv")
-
 munis.xy <- as.tibble(extractXYZ(munis.r))
 LC.xy <- as.tibble(extractXYZ(landCover.r))
 Lpr.xy <- as.tibble(extractXYZ(Lpr.r))
@@ -94,8 +89,17 @@ slope.xy <- as.tibble(extractXYZ(slope.r))
 agri.xy <- as.tibble(extractXYZ(agri.r))
 infra.xy <- as.tibble(extractXYZ(infra.r))
 
+#create function to do each of above:
+#1. load raster
+#2. flip
+#3. extract and return as tibble
 
-#joins (because some maps are not perfectly aligned with munis.r
+
+
+
+
+
+#joins (because Infrastructure and Land Price maps are not perfectly aligned with munis.r)
 join.xy <- left_join(munis.xy, infra.xy, by = c("row", "col")) %>%
   select(-V1.y) %>%
   rename(muniID = vals.x, Infrastructure = vals.y) %>%
@@ -105,34 +109,20 @@ join.xy <- left_join(munis.xy, infra.xy, by = c("row", "col")) %>%
   filter_all(all_vars(!is.na(.)))      #filter any rows missing data NA values
 
 
-region.xy <- join.xy %>%
-  rename(" " = V1, Y = row.x, X = col.x) %>%
-  mutate(agentID = row_number()) %>%  #add dummy agent_ID
-  mutate(BT = "Cognitor") %>%
-  
-  ##FR here
-  
-  mutate(Agriculture = agri.xy$vals)  %>%
+#DoubleCropping (used for other variables below, but ultimately removed?)
+state <-read_csv("Data/Municipality_area_and_IBGE_code_number.csv")
+#Fstate_vals <- c(17,	29,	31,	35,	41,	42,	43,	50,	51,	52)
 
-##Nature here
-  
-  mutate(Human = 1) %>%
-  mutate(Development = 1) %>%
-  mutate(Economic = 1) %>%
-  
-#Acessibility	
-#Climate	     #not needed??
-
-
-#Growing Season	
-#Other Agriculture	
-#Other	
-#Land Protection
+DCstates <- c(50,51,41,52,31,35)  #specify which states double cropping is possible in
+DC <- inner_join(join.xy, state, by = c("muniID" = "CD_GCMUN")) %>%
+  select(row, col, CD_GCUF) %>%
+  mutate("Double Cropping" = if_else(CD_GCUF %in% DCstates,  1, 0)) 
+      
+join.xy <- left_join(join.xy, DC.s, by = c("row","col")) %>%
+  select(-CD_GCUF)
   
 
-
-
-
+#LC here... this code will be useful
 munis.xy <- munis.xy %>%
   mutate(LC = if_else(LCa == "LC1area", 1, 
       if_else(LCa == "LC2area", 2,
@@ -143,9 +133,26 @@ munis.xy <- munis.xy %>%
 
 
 
+#add columns that are either uniform or simple row number
+region.xy <- join.xy %>%
+  rename(" " = V1, Y = row.x, X = col.x) %>%
+  mutate(agentID = row_number()) %>%  #add dummy agent_ID
+  mutate(BT = "Cognitor") %>%
+  mutate(Agriculture = agri.xy$vals)  %>%
+  mutate(Human = 1) %>%
+  mutate(Development = 1) %>%
+  mutate(Economic = 1) 
+  
+#Acessibility	
+#Climate	     #not needed??
 
 
-
+#Growing Season	
+#Other Agriculture	
+#Other	
+#Land Protection
+  
+#old code below. comment out as implemented above
 muniscsv$FR[muniscsv$Landuse==1]<-5
 muniscsv$FR[muniscsv$Landuse==2]<-6
 muniscsv$FR[muniscsv$Landuse==3]<-sample(1:3, nrow(muniscsv), replace=T)
@@ -188,25 +195,25 @@ muniscsv$Other<-0
 muniscsv$Other[muniscsv$FR=="FR7"]<-1
 muniscsv$"Land Protection"<-1
 #muniscsv$`Land Price`<-signif(muniscsv$`Land Price`, digits=3)
-muniscsv$`Land Price`<-1500-muniscsv$`Land Price`
-muniscsv$`Land Price`<-muniscsv$`Land Price`/1500
+#muniscsv$`Land Price`<-1500-muniscsv$`Land Price`
+#muniscsv$`Land Price`<-muniscsv$`Land Price`/1500
 muniscsv$Ports<-NULL
 muniscsv$Soil<-NULL
 muniscsv$Slope<-NULL
 muniscsv$Landuse<-NULL
-names(statecsv)[5]="muniID"
-bzro<-inner_join(statecsv, muniscsv, by = "muniID")
-statedata<-bzro[, c(2,5)]
-statedata$DoubleCropping<-0
-statedata$DoubleCropping[statedata$CD_GCUF==50]<-1
-statedata$DoubleCropping[statedata$CD_GCUF==51]<-1
-statedata$DoubleCropping[statedata$CD_GCUF==41]<-1
-statedata$DoubleCropping[statedata$CD_GCUF==52]<-1
-statedata$DoubleCropping[statedata$CD_GCUF==31]<-1
-statedata$DoubleCropping[statedata$CD_GCUF==35]<-1
-statedata$CD_GCUF<-NULL
-statedata<-statedata[!duplicated(statedata$muniID),]
-muniscsv<-inner_join(muniscsv, statedata, by = "muniID")
+#names(statecsv)[5]="muniID"
+#bzro<-inner_join(statecsv, muniscsv, by = "muniID")
+#statedata<-bzro[, c(2,5)]
+#statedata$DoubleCropping<-0
+#statedata$DoubleCropping[statedata$CD_GCUF==50]<-1
+#statedata$DoubleCropping[statedata$CD_GCUF==51]<-1
+#statedata$DoubleCropping[statedata$CD_GCUF==41]<-1
+#statedata$DoubleCropping[statedata$CD_GCUF==52]<-1
+#statedata$DoubleCropping[statedata$CD_GCUF==31]<-1
+#statedata$DoubleCropping[statedata$CD_GCUF==35]<-1
+#statedata$CD_GCUF<-NULL
+#statedata<-statedata[!duplicated(statedata$muniID),]
+#muniscsv<-inner_join(muniscsv, statedata, by = "muniID")
 
 names(muniscsv)[1]=""
 muniscsv$`Growing Season`[muniscsv$DoubleCropping==1]<-1
