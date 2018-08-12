f901a5175a8026c7a63fdd1dc5120b909953f2eb
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-08-12 10:47:18 +0100

    account for land cover in prize (not natural <- 1)

--- LandPriceMap.R
+++ LandPriceMap.R
@@ -7,12 +7,16 @@ library(raster)
 library(sf)
 
 
+#unzip and read files
 unzip(zipfile="Data/LandPrice.zip",exdir="Data/LandPrice")  #unzip
 
 unzip(zipfile="Data/sim10_BRmunis_latlon_5km_2018-04-27.zip",exdir="Data")  #unzip
 munis.r <- raster("Data/sim10_BRmunis_latlon_5km_2018-04-27.asc")
 
 Landprice<- st_read("Data/LandPrice/Native_vegetation.shp")
+lc2001 <- raster("Data/ObservedLCmaps/brazillc_2001_PastureB.asc")
+
+#rasterise year 2001
 Lp2001<- Landprice[,-(4:19)]
 Lp2001<- Lp2001[,-(1:2)]
 Lpr<-rasterize(Lp2001, munis.r, field='X2001')  
@@ -21,11 +25,13 @@ Lpr<-rasterize(Lp2001, munis.r, field='X2001')
 Lpr.scaled <- Lpr
 values(Lpr.scaled)= round((1500-(values(Lpr)))/1500, digits=3)
 
-plot(Lpr.scaled)
+#where land cover is not nature, price should be 1 
+Lpr.scaled[lc2001 != 1] <- 1
 
-
+#output data
 writeRaster(Lpr, "Data/LandPrice2001.asc", format = 'ascii', overwrite=T)
-writeRaster(Lpr.scaled, "Data/LandPrice2001_Capital.asc", format = 'ascii', overwrite=T)
+writeRaster(Lpr.scaled, "Data/LandPrice2001_Capital_nat1.asc", format = 'ascii', overwrite=T)
 
+#remove unzipped files
 unlink("Data/sim10_BRmunis_latlon_5km_2018-04-27.asc")
 unlink("Data/LandPrice", recursive = T)
