9a8a3cf14e4c53dcdc6dcbfa07a26edb84473501
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-08-21 17:29:36 +0100

    Added indigenous lands

--- LandProtectionMap.r
+++ LandProtectionMap.r
@@ -15,6 +15,11 @@ LandProtect<- st_read("Data/landProtection/protected_areas_clip.shp")
 Lp2001<- LandProtect["FID_ucsmi"]
 Lpr<-rasterize(Lp2001, munis.r, field='FID_ucsmi')  
 
+#Indigenous is for all services
+Indigenous <- st_read("Data/landProtection/indigenous.shp")
+Indigenous <- Indigenous["GID0"]
+Indig <-rasterize(Indigenous, munis.r, field='GID0')  
+
 #Amazon is for Soy and Pasture 
 Amazon <- st_read("Data/landProtection/Amazon_area.shp")
 Amazon <- Amazon['NOME']
@@ -26,6 +31,9 @@ LP_bin <- Lpr
 LP_bin[!is.na(Lpr)] <- 0
 LP_bin[is.na(Lpr)] <- 1
 
+#add indig at 
+LP_bin[Indig == 1] <- 0.1 
+
 #LandProtect currently is the only restriction on OAgri and Maize
 #output data
 writeRaster(LP_bin, "Data/All_ProtectionMap.asc", format = 'ascii', overwrite=T)
@@ -36,13 +44,12 @@ lc2006 <- raster("Data/ObservedLCmaps/brazillc_2006_PastureB.asc")
 #where 2006 nature AND Amazon Biome, Soy should be prevented
 Soy_bin <- Amzn
 Soy_bin[lc2006 != 1] <- NA
+Soy_bin[!is.na(Soy_bin)] <- 0  #protected
+Soy_bin[is.na(Soy_bin)] <- 1   #not protected
 
 #then combine with 'standard' land protection
-Soy_bin[LP_bin == 0] <- 1
-
-#reverse and set NAs to 1
-Soy_bin[Soy_bin == 1] <- 0 #reverse so protected is 0
-Soy_bin[is.na(Soy_bin)] <- 1  #set NAs to 1 (unprotected)
+Soy_bin[LP_bin == 0] <- 0      #protected
+Soy_bin[LP_bin == 0.1] <- 0.1  #indigenous
 
 #output data
 writeRaster(Soy_bin, "Data/Soy_ProtectionMap_2006.asc", format = 'ascii', overwrite=T)
@@ -53,13 +60,12 @@ lc2009 <- raster("Data/ObservedLCmaps/brazillc_2009_PastureB.asc")
 #where 2008 nature AND Amazon Biome, Pature should be prevented
 Pas_bin <- Amzn
 Pas_bin[lc2009 != 1] <- NA
+Pas_bin[!is.na(Pas_bin)] <- 0  #protected
+Pas_bin[is.na(Pas_bin)] <- 1   #not protected
 
 #then combine with 'standard' land protection
-Pas_bin[LP_bin == 0] <- 1
-
-#reverse and set NAs to 1
-Pas_bin[Pas_bin == 1] <- 0 #reverse so protected is 0
-Pas_bin[is.na(Pas_bin)] <- 1  #set NAs to 1 (unprotected)
+Pas_bin[LP_bin == 0] <- 0      #protected
+Pas_bin[LP_bin == 0.1] <- 0.1  #indigenous
 
 #output data
 writeRaster(Pas_bin, "Data/Pasture_ProtectionMap_2009.asc", format = 'ascii', overwrite=T)
