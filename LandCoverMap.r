500378a5f5f735d59b9d38d6dc842d9134009617
jamesdamillington@gmail.com <jamesdamillington@gmail.com>
2018-09-11 19:40:30 +0100

    Improved conversions logic. Moved differences code to a function.

--- LandCoverMap.r
+++ LandCoverMap.r
@@ -6,7 +6,7 @@ library(raster)
 library(tidyverse)
 library(readxl)
 
-#raster to xyz  (with help from https://stackoverflow.com/a/19847419)
+#function converts raster to xyz  (with help from https://stackoverflow.com/a/19847419)
 #sepcify input raster, whether nodata cells should be output, whether a unique cell ID should be added
 #return is a matrix. note format is row (Y) then col (X)
 extractXYZ <- function(raster, nodata = FALSE, addCellID = TRUE){
@@ -27,8 +27,7 @@ extractXYZ <- function(raster, nodata = FALSE, addCellID = TRUE){
 }
 
 
-
-#converts data in CRAFTY output file for a single variable and creates a raster
+#function converts data in CRAFTY output file for a single variable and creates a raster
 outputRaster <- function(data, variable){
   
   out <- data %>%
@@ -41,138 +40,267 @@ outputRaster <- function(data, variable){
 }
 
 
-#function() for each municipality:
-#- compare A obs and desired; change if necessary (increase from OA, decrease to OA)
-#- compare OA obs and desired; change if necessary (increase from Pas, decrease to Pas)
-#convs is the conversions calculated above, lcs is the lc_munis df
-#both should already have been subst to a single muni
-convertLCs <- function(convs, lcs) {
-
-  #subset data
-  NA_lcs <- filter(lcs, lc == 1 | lc == 4)  #not Agri or Pas
+#function to Calculate number of OAgri, Agri and Pasture cells needed to match OA_plant and A_plant
+calcDiffcs <- function(tbl) {
   
-  P_lcs  <- filter(lcs, lc == 5) 
-  A_lcs <- filter(lcs, lc == 3)
-  OA_lcs <- filter(lcs, lc == 2)
-
-  A_obs <- length(A_lcs$lc)        #for reporting
-  A_diffc = convs$A_final - A_obs  #counter of how many conversions needed
-
-  # print(paste0("A_obs: ",A_obs))
-  # print(paste0("A_target: ",convs$A_final))
-  # print(paste0("A_diffc: ",A_diffc))
+   # Calculate number of OAgri, Agri and Pasture cells needed to match OA_plant and A_plant:
+  #Overall A_final + OA_final + P_final must equal A_mapped + OA_mapped
+  #So:
+  #case 1
+  #if OA_mapped > OA_planted AND A_mapped < A_planted
+    #then take enough from difference so A_mapped == A_planted, any remainder is pasture 
+  #case 2
+  #if OA_mapped > OA_planted AND A_mapped >= A_planted
+    #then OA is planted value and remainder is pasture, A_mapped does not change
+  #case 3
+  #if OA_mapped == OA_planted AND A_mapped < A_planted
+    #then nothing changes
+  #case 4
+  #if OA_mapped == OA_planted AND A_mapped >= A_planted
+    #then nothing changes
+  #case 5
+  #if OA_mapped < OA_planted AND A_mapped <= A_planted
+    #then nothing changes
+  #case 6
+  #if OA_mapped < OA_planted AND A_mapped > A_planted
+    #then add difference from A to OA (so that OA_M == OA_p, or until A_m == A_p) 
   
-  #print(A_diffc)
-  #print(is.integer(A_diffc))
-  #print(is.na(A_diffc))
   
-  ctr <- 1 #counter to ensure we don't try to access beyond length of tables below
+  #calculate differences (used in cases below)
+  tbl <- tbl %>%
+    dplyr::select(muniID, A_mapped_cells, OA_mapped_cells, A_plant_cells, OA_plant_cells) %>%
+    mutate(A_diffc = A_mapped_cells - A_plant_cells) %>%
+    mutate(OA_diffc = OA_mapped_cells - OA_plant_cells)
   
-  # print(OA_lcs)
+  #case 1
+  #if OA_mapped > OA_planted AND A_mapped < A_planted
+    #then take enough from difference so A_mapped == A_planted, any remainder is pasture
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc > 0 & A_diffc < 0, OA_plant_cells,99))  %>%  
+    mutate(A_final = 
+      if_else(OA_diffc > 0 & A_diffc < 0, 
+        if_else(OA_diffc >= abs(A_diffc), A_plant_cells, A_mapped_cells + OA_diffc),
+        99)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc > 0 & A_diffc < 0,
+        if_else(OA_diffc >= abs(A_diffc), OA_mapped_cells - OA_plant_cells - abs(A_diffc), 0),
+        99))
   
-  #increase A from OA (2 -> 3)
-  if(!is.na(A_diffc))  #in case where there are no A pixels, A_diffc is NA 
-  {
-    while(A_diffc > 0) {
-      if(!any(2 %in% OA_lcs$lc)) break   #if there are no values to convert, break
-      if(ctr > length(OA_lcs$lc)) break  #if we are trying to convert more values than available, break
-      
-      OA_lcs[ctr,4] <- 3                 #do the conversion
-     
-      ctr <- ctr + 1                     #update counter
-      A_diffc <- A_diffc - 1             #update counter
-    }  
-    
-    # print(OA_lcs)
-    # 
-    # print(A_lcs)
-
-    ctr <- 1
-    #decrease A to OA (3 -> 2)
-    while(A_diffc < 0) {
-      if(!any(3 %in% A_lcs$lc)) break
-      if(ctr > length(A_lcs$lc)) break
-      
-      A_lcs[ctr,4] <- 2
-      
-      ctr <- ctr + 1
-      A_diffc <- A_diffc + 1
-    }
-  }
+  #case 2
+  #if OA_mapped > OA_planted AND A_mapped >= A_planted
+    #then OA is planted value and remainder is pasture, A_mapped does not change
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc > 0 & A_diffc >= 0, OA_plant_cells, OA_final)) %>%
+    mutate(A_final = 
+        if_else(OA_diffc > 0 & A_diffc >= 0, A_plant_cells, A_final)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc > 0 & A_diffc >= 0, OA_diffc, P_final))
   
-  # print(A_lcs)
+  #case 6
+  #if OA_mapped < OA_planted AND A_mapped > A_planted
+    #then add difference from A to OA:
+      #if A_diffc <= abs(OA_diffc) OA_final is max possible, otherwise OA_plant (A_final is always A_mapped - A_diffc)
+      #nothing goes to P_final
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc < 0 & A_diffc > 0,
+        if_else(A_diffc <= abs(OA_diffc), OA_mapped_cells + A_diffc, OA_mapped_cells + abs(OA_diffc)),
+        OA_final)) %>%
+    mutate(A_final = 
+      if_else(OA_diffc < 0 & A_diffc > 0, 
+        if_else(A_diffc <= abs(OA_diffc), A_mapped_cells - A_diffc, A_mapped_cells - abs(OA_diffc)),
+        A_final)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc < 0 & A_diffc > 0, 0, P_final))
   
-  #update data as we may have made some conversions that have changed OA_lcs (makes OA conversions below more accurate)
-  olcs <- bind_rows(A_lcs, OA_lcs, P_lcs)
-  #NA_lcs <- filter(olcs, lc == 1 | lc == 4)  #not Agri or Pas
-  P_lcs  <- filter(olcs, lc == 5) 
-  A_lcs <- filter(olcs, lc == 3)
-  OA_lcs <- filter(olcs, lc == 2)
-
-  OA_obs <- length(OA_lcs$lc)
-  OA_diffc = convs$OA_final - OA_obs
-
-  # print(paste0("OA_obs: ",OA_obs))
-  # print(paste0("OA_target: ",convs$OA_final))
-  # print(paste0("OA_diffc: ",OA_diffc))
-  # print(OA_lcs)
   
-  ctr <- 1
+  #case 3
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc == 0 & A_diffc < 0, 0, OA_final)) %>%
+    mutate(A_final = 
+      if_else(OA_diffc == 0 & A_diffc < 0, 0, A_final)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc == 0 & A_diffc < 0, 0, P_final))
   
-  #increase OA from Pas (5 -> 2)
-  if(!is.na(OA_diffc))  #in case where there are no OA pixels, OA_diffc is NA
-  {
-    while(OA_diffc > 0) {
-      if(!any(5 %in% P_lcs$lc)) break
-      if(ctr > length(P_lcs$lc)) break
-      
-      P_lcs[ctr,4] <- 2
-      
-      ctr <- ctr + 1
-      OA_diffc <- OA_diffc - 1
-    }
+  #case 4
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc == 0 & A_diffc >= 0, 0, OA_final)) %>%
+    mutate(A_final = 
+      if_else(OA_diffc == 0 & A_diffc >= 0, 0, A_final)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc == 0 & A_diffc >= 0, 0, P_final))
   
-    ctr <- 1
-    #decrease OA to Pas (2 -> 5)
-    while(OA_diffc < 0) {
-      if(!any(2 %in% OA_lcs$lc)) break
-      if(ctr > length(OA_lcs$lc)) break
-      
-      OA_lcs[ctr,4] <- 5
-      
-      ctr <- ctr + 1
-      OA_diffc <- OA_diffc + 1
-    }
-  }
+  #case 5
+  tbl <- tbl %>%
+    mutate(OA_final = 
+      if_else(OA_diffc < 0 & A_diffc <= 0, 0, OA_final)) %>%
+    mutate(A_final = 
+      if_else(OA_diffc < 0 & A_diffc <= 0, 0, A_final)) %>%
+    mutate(P_final = 
+      if_else(OA_diffc < 0 & A_diffc <= 0, 0, P_final))
   
-  # print(OA_lcs)
+  tbl <- tbl %>%
+    mutate(Agri_final = A_final + OA_final)
+ 
+  return(tbl)
   
-  olcs <- bind_rows(NA_lcs, A_lcs, OA_lcs, P_lcs)
+  #check all cells have changed
+  #k <- j %>%
+  #  filter(OA_final == 99)
   
-  #used during testing
-  # if(length(lcs$lc) != length(olcs$lc))
-  # {  
-  #   print(paste0("lcs: ", length(lcs$lc)))
-  #   print(lcs)
-  #   
-  #   print(paste0("olcs: ", length(olcs$lc)))
-  #   print(olcs)
-  #   
-  # }
-
-    
-  # print(paste0("A_obs: ",length(filter(olcs, lc == 3)[,4])))
-  # print(paste0("A_target: ",convs$A_final))
-  # print(paste0("OA_obs: ",length(filter(olcs, lc == 2)[,4])))
-  # print(paste0("OA_target: ",convs$OA_final))
   
-  # print(olcs)
-  
-  return(olcs)
 }
 
 
-yrs <- seq(2006, 2015, 1)   #maps made for all these years
+#function() for each municipality:
+#- compare A obs and desired; change if necessary (increase from OA, decrease to OA)
+#- compare OA obs and desired; change if necessary (increase from Pas, decrease to Pas)
+#convs is the conversions calculated in calcDiffcs, lcs is the lc_munis df
+#both should already have been subst to a single muni
+convertLCs <- function(convs, lcs) {
+
+  #print(convs[,8:11])
+  #print(lcs)
+  
+  #subset data
+  NA_lcs <- filter(lcs, lc == 1 | lc == 4)  #not Agri or Pas
+  Agri_lcs <- filter(lcs, lc == 2 | lc == 3)   #Agri (not Pas or Nat)
+  P_lcs  <- filter(lcs, lc == 5)  #Pas
+  A_lcs <- filter(lcs, lc == 3)   #soy/maize
+  OA_lcs <- filter(lcs, lc == 2)  #OA
+
+  #calc how many conversions needed (if negative move to pasture) 
+  Agri_obs <- length(Agri_lcs$lc)        
+  Agri_diffc <- Agri_obs - convs$Agri_final
+
+  # print(convs)
+  # print(Agri_obs)
+  # print(convs$Agri_final)
+  # print(Agri_diffc)
+  # print(is.na(Agri_diffc))
+  # print(length(convs$Agri_final))
+  
+  #print(OA_lcs)
+  ctr <- 1 #counter to ensure we don't try to access beyond length of tables below
+
+  #if(length(OA_lcs$lc) == 0) print("length(OA_lcs$lc) == 0")
+  
+  #if we observe more Agri than we want, move OA to pasture
+  #try to move sufficient amount to get the right amount of Agri, otherwise all OA
+  if(length(OA_lcs$lc) > 0)  #if there are some Agri pixels
+  {
+    while(Agri_diffc > 0) {
+      if(!any(2 %in% OA_lcs$lc)) break   #if there are no OA values to convert, break
+      if(ctr > length(OA_lcs$lc)) break  #should never happen but check if we are trying to convert more values than available, break
+      
+      OA_lcs[ctr,4] <- 5                 #do the conversion to Pas
+      #print("update: 2 -> 5")
+      
+      ctr <- ctr + 1                     #update counter
+      Agri_diffc <- Agri_diffc - 1       #update counter
+    }
+  }
+  
+  #print(OA_lcs)
+  #update data as we may have made some conversions that have changed OA_lcs 
+  nlcs <- bind_rows(NA_lcs, A_lcs, OA_lcs, P_lcs)
+  
+  #split out again for later
+  NA_lcs <- filter(nlcs, lc == 1 | lc == 4)  #not Agri or Pas
+  Agri_lcs <- filter(nlcs, lc == 2 | lc == 3)   #Agri (not Pas or Nat)
+  P_lcs  <- filter(nlcs, lc == 5)  #Pas
+  A_lcs <- filter(nlcs, lc == 3)   #soy/maize
+  OA_lcs <- filter(nlcs, lc == 2)  #OA
+  
+  #print("nlcs")
+  #print(nlcs)
+  #if(length(Agri_lcs$lc) == 0) print("length(Agri_lcs$lc) == 0")
+  
+  #now distribute amongst soy/maize (A) vs OA (only if there are any Agri cells)
+  if(length(Agri_lcs$lc) > 0) {
+    
+    #calculate desired soy/maize prop
+    Agri_sum <- convs$A_final + convs$OA_final  #total number of desired Agri cells, used to calc prop
+    A_prop <- NA
+    
+    #if both A and OA have a value we need to calc prop, otherwise we can assign based on 0s
+    if(convs$A_final > 0 & convs$OA_final > 0) { 
+      A_prop <- convs$A_final / Agri_sum
+    } else if(convs$A_final > 0) {
+      A_prop <- 1.0
+    } else { 
+      A_prop <- 0
+    }
+  
+    #print(paste0("A_prop: ", A_prop))
+      
+    #use desired prop to calc desired soy/maize cells
+    A_obs <- length(A_lcs$lc) 
+    OA_obs <- length(OA_lcs$lc) 
+    
+    if(is.na(length(A_lcs$lc))) A_obs <- 0
+    if(is.na(length(OA_lcs$lc))) OA_obs <- 0
+    
+    Agri_obs <- A_obs + OA_obs
+
+    #desired number of soy/maize cells
+    A_dobs <- round(Agri_obs * A_prop, 0)
+    
+    #check difference between desired and observed
+    A_diffc <- A_dobs - A_obs
+
+    #print(paste0("A_diffc: ", A_diffc))
+    
+    ctr <- 1 #counter to ensure we don't try to access beyond length of tables below
+    
+    #print(OA_lcs)
+    
+    #if desired soy/maize is greater than observed, take some from OA
+    while(A_diffc > 0) {
+      if(!any(2 %in% OA_lcs$lc)) break   #if there are no OA values to convert (because we already changed them all), break
+      if(ctr > length(OA_lcs$lc)) break  #should never happen but check if we are trying to convert more values than available, break
+      
+      OA_lcs[ctr,4] <- 3                 #do the conversion to soy/maize
+      #print("update: 2 -> 3")
+      
+      ctr <- ctr + 1               #update counter
+      A_diffc <- A_diffc - 1       #update counter
+    }
+    
+    #print(OA_lcs)
+    
+    ctr <- 1 #counter to ensure we don't try to access beyond length of tables below
+    
+    #print(A_lcs)
+    
+    #if desired soy/maize is less than observed, give some to OA
+    while(A_diffc < 0) {
+      if(!any(3 %in% A_lcs$lc)) break   #if there are no A values to convert (because we already changed them all), break
+      if(ctr > length(A_lcs$lc)) break  #should never happen but check if we are trying to convert more values than available, break
+      
+      A_lcs[ctr,4] <- 2                 #do the conversion to other agri
+      #print("update: 3 -> 2")
+      
+      ctr <- ctr + 1               #update counter
+      A_diffc <- A_diffc + 1       #update counter
+    }
+    
+    #print(A_lcs)
+  }
+  
+  #update data as we may have made some conversions  
+  nlcs <- bind_rows(NA_lcs, A_lcs, OA_lcs, P_lcs)
+  
+  return(nlcs)
+  
+}
+
+
+yrs <- seq(2005, 2015, 1)   #maps made for all these years
 
 for(yr in seq_along(yrs)){
 
@@ -240,111 +368,9 @@ for(yr in seq_along(yrs)){
   #missing <- joined %>% 
   #  filter(is.na(A_plant_cells))
   
-  
-  
-  # Calculate number of OAgri, Agri and Pasture cells needed to match OA_plant and A_plant:
-  #Overall A_final + OA_final + P_final must equal A_mapped + OA_mapped
-  #So:
-  #case 1
-  #if OA_mapped > OA_planted AND A_mapped < A_planted
-    #then take enough from difference so A_mapped == A_planted, any remainder is pasture 
-  #case 2
-  #if OA_mapped > OA_planted AND A_mapped >= A_planted
-    #then OA is planted value and remainder is pasture, A_mapped does not change
-  #case 3
-  #if OA_mapped == OA_planted AND A_mapped < A_planted
-    #then nothing changes
-  #case 4
-  #if OA_mapped == OA_planted AND A_mapped >= A_planted
-    #then nothing changes
-  #case 5
-  #if OA_mapped < OA_planted AND A_mapped <= A_planted
-    #then nothing changes
-  #case 6
-  #if OA_mapped < OA_planted AND A_mapped > A_planted
-    #then add difference from A to OA (so that OA_M == OA_p, or until A_m == A_p) 
-  
-  
-  #calculate differences (used in cases below)
-  joined <- joined %>%
-    dplyr::select(muniID, A_mapped_cells, OA_mapped_cells, A_plant_cells, OA_plant_cells) %>%
-    mutate(A_diffc = A_mapped_cells - A_plant_cells) %>%
-    mutate(OA_diffc = OA_mapped_cells - OA_plant_cells)
-  
-  #case 1
-  #if OA_mapped > OA_planted AND A_mapped < A_planted
-    #then take enough from difference so A_mapped == A_planted, any remainder is pasture
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc > 0 & A_diffc < 0, OA_plant_cells,99))  %>%  
-    mutate(A_final = 
-      if_else(OA_diffc > 0 & A_diffc < 0, 
-        if_else(OA_diffc >= abs(A_diffc), A_plant_cells, A_mapped_cells + OA_diffc),
-        99)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc > 0 & A_diffc < 0,
-        if_else(OA_diffc >= abs(A_diffc), OA_mapped_cells - OA_plant_cells - abs(A_diffc), 0),
-        99))
-  
-  #case 2
-  #if OA_mapped > OA_planted AND A_mapped >= A_planted
-    #then OA is planted value and remainder is pasture, A_mapped does not change
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc > 0 & A_diffc >= 0, OA_plant_cells, OA_final)) %>%
-    mutate(A_final = 
-        if_else(OA_diffc > 0 & A_diffc >= 0, A_plant_cells, A_final)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc > 0 & A_diffc >= 0, OA_diffc, P_final))
-  
-  #case 6
-  #if OA_mapped < OA_planted AND A_mapped > A_planted
-    #then add difference from A to OA:
-      #if A_diffc <= abs(OA_diffc) OA_final is max possible, otherwise OA_plant (A_final is always A_mapped - A_diffc)
-      #nothing goes to P_final
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc < 0 & A_diffc > 0,
-        if_else(A_diffc <= abs(OA_diffc), OA_mapped_cells + A_diffc, OA_mapped_cells + abs(OA_diffc)),
-        OA_final)) %>%
-    mutate(A_final = 
-      if_else(OA_diffc < 0 & A_diffc > 0, 
-        if_else(A_diffc <= abs(OA_diffc), A_mapped_cells - A_diffc, A_mapped_cells - abs(OA_diffc)),
-        A_final)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc < 0 & A_diffc > 0, 0, P_final))
-  
-  
-  #case 3
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc == 0 & A_diffc < 0, 0, OA_final)) %>%
-    mutate(A_final = 
-      if_else(OA_diffc == 0 & A_diffc < 0, 0, A_final)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc == 0 & A_diffc < 0, 0, P_final))
-  
-  #case 4
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc == 0 & A_diffc >= 0, 0, OA_final)) %>%
-    mutate(A_final = 
-      if_else(OA_diffc == 0 & A_diffc >= 0, 0, A_final)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc == 0 & A_diffc >= 0, 0, P_final))
-  
-  #case 5
-  joined <- joined %>%
-    mutate(OA_final = 
-      if_else(OA_diffc < 0 & A_diffc <= 0, 0, OA_final)) %>%
-    mutate(A_final = 
-      if_else(OA_diffc < 0 & A_diffc <= 0, 0, A_final)) %>%
-    mutate(P_final = 
-      if_else(OA_diffc < 0 & A_diffc <= 0, 0, P_final))
-  
-  #check all cells have changed
-  #k <- j %>%
-  #  filter(OA_final == 99)
+  #calculate differences between mapped and planted areas (in cells)
+  diffs <- calcDiffcs(joined)
+  diffs <- diffs %>% filter(!is.na(A_plant_cells))  #drop NA
   
   #now update map
   #read muniID map -> get x,y,z
@@ -379,29 +405,34 @@ for(yr in seq_along(yrs)){
   final <- data.frame() 
 
   #for testing
-  #dummy <- c(3527603,3527603,3527504,3527504,3528205)
+  dummy <- c(3527603,3527603,3527504,3527504,3528205)
     
   #loop through all munis to update https://stackoverflow.com/a/13916342/10219907
-  for(i in 1:length(unique(lc_munis$muniID))) {
+  for(i in 1:length(unique(diffs$muniID))) {
   
   #for(i in 1:length(unique(dummy))) {  
     
     #i <- 1 # for testing
     #this.muniID <- unique(dummy)[i]
 
-    this.muniID <- unique(lc_munis$muniID)[i]
+    this.muniID <- unique(diffs$muniID)[i]
     
     lcm <- filter(lc_munis, muniID == this.muniID)
-    js <- filter(joined, muniID == this.muniID)
+    js <- filter(diffs, muniID == this.muniID)
      
     this.conv <- convertLCs(js, lcm)
     
-    print(this.muniID)
+    #print(this.muniID)
+    #print(lcm)
+    #print(this.conv)
     
     if(i == 1) final <- this.conv
     else final <- bind_rows(final, this.conv)
 
   }
+  
+  
+#test <- left_join(final, lc_munis, by = c("row" = "row", "col" = "col"))  
   
   #set final to a raster with same extent as inputs (to the same)with help from https://gis.stackexchange.com/questions/250149/assign-values-to-a-subset-of-cells-of-a-raster)
   final.r <- raster(munis.r)
